37a241b98da3ef122df030aafa4e2adef18d47a6
sharan elumalai <109457528+sharanelumalai@users.noreply.github.com>
2026-09-14 14:39:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2603,7 +2603,7 @@ const cameraTune = {
 0: { zoom: 1, panX: 0, panY: 0 },
 1: { zoom: 0.98, panX: 0, panY: 0 },   // ch2: nudged left
 2: { zoom: 1, panX: 0, panY: 0 },    // ch3: nudged left
-3: { zoom: 0.2, panX: 0.10, panY: 0.2 },   // ch4: nudged left (was 0.24)
+3: { zoom: 0.6, panX: 0.10, panY: 0.2 },   // ch4: nudged left (was 0.24)
 4: { zoom: 0.85, panX: -0.12, panY: 0.10 }, // ch5: left + zoomed out + moved down
 5: { zoom: 0.6, panX: 0.12, panY: 0.05 },
 6: { zoom: 1.6, panX: 0, panY: 0 },        // ch7: zoomed out to bring the floating rocks into frame
