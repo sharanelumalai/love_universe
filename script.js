a10ea0a92b55ac3725cf4659ab34f0990c156f49
sharan elumalai <109457528+sharanelumalai@users.noreply.github.com>
2026-09-14 22:58:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2604,7 +2604,7 @@ const cameraTune = {
 1: { zoom: 0.98, panX: 0, panY: 0 },   // ch2: nudged left
 2: { zoom: 1, panX: 0, panY: 0 },    // ch3: nudged left
 3: { zoom: 0.5, panX: 0, panY: 0.2 },   // ch4: nudged left (was 0.24)
-4: { zoom: 0.6, panX: 0, panY: 0.10 }, // ch5: left + zoomed out + moved down
+4: { zoom: 0.7, panX: 0, panY: 0.8 }, // ch5: left + zoomed out + moved down
 5: { zoom: 0.6, panX: 0, panY: 0.10 },
 6: { zoom: 1.6, panX: 0, panY: 0 },        // ch7: zoomed out to bring the floating rocks into frame
 7: { zoom: 1, panX: -0.12, panY: 0 },      // ch8: nudged left
@@ -2623,7 +2623,7 @@ const modelTune = { 0:{rotY:0}, 1:{rotY:0}, 2:{rotY:3.14}, 3:{rotY:-0.57}, 4:{ro
 
 // Per-chapter background image placement: x/y shift and scale.
 const bgTune = { 0:{bgX:-0.5,bgY:0,bgScale:2}, 1:{bgX:-0.1,bgY:-1,bgScale:1}, 2:{bgX:-0.2,bgY:0,bgScale:1.1},
-                 3:{bgX:-0.2,bgY:0,bgScale:1}, 4:{bgX:0,bgY:0,bgScale:1},
+                 3:{bgX:-1,bgY:0,bgScale:1}, 4:{bgX:0,bgY:0,bgScale:1},
 5:{bgX:0,bgY:0,bgScale:1}, 6:{bgX:0,bgY:0.9,bgScale:1},
 7:{bgX:0.9,bgY:0,bgScale:1.05}, 8:{bgX:0.8,bgY:0.9,bgScale:1.3},
 9:{bgX:0,bgY:0,bgScale:1}, 10:{bgX:2,bgY:0,bgScale:1.15}, 11:{bgX:-0.9,bgY:2,bgScale:1.2},
