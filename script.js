1a0854b1ab3dc7b0b4708d3575bb8ff2d4a314fd
sharan elumalai <109457528+sharanelumalai@users.noreply.github.com>
2026-09-14 14:18:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2622,7 +2622,7 @@ const modelTune = { 0:{rotY:0}, 1:{rotY:0}, 2:{rotY:3.14}, 3:{rotY:-0.57}, 4:{ro
 9:{rotY:0}, 10:{rotY:3.16}, 11:{rotY:3.16}, 12:{rotY:0}, 13:{rotY:0} };
 
 // Per-chapter background image placement: x/y shift and scale.
-const bgTune = { 0:{bgX:-0.5,bgY:0,bgScale:2}, 1:{bgX:0.5,bgY:-1,bgScale:1}, 2:{bgX:0.4,bgY:0,bgScale:1},
+const bgTune = { 0:{bgX:-0.5,bgY:0,bgScale:2}, 1:{bgX:0.5,bgY:-1,bgScale:1}, 2:{bgX:0.12,bgY:0,bgScale:1},
                  3:{bgX:0,bgY:-2.25,bgScale:1}, 4:{bgX:0,bgY:0,bgScale:1},
 5:{bgX:0,bgY:0,bgScale:1}, 6:{bgX:0,bgY:0.9,bgScale:1},
 7:{bgX:0.9,bgY:0,bgScale:1.05}, 8:{bgX:0.8,bgY:0.9,bgScale:1.3},
