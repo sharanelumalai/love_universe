ab3f173ba953157cb33005428582fef65888c935
sharan elumalai <109457528+sharanelumalai@users.noreply.github.com>
2026-09-15 09:05:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1653,8 +1653,8 @@ const partTune = {
 // Adjust live with the P key panel, then paste the printed values here.
 6: {
 center:    { x: 0,    y: 0,    z: 0, s: 1,    rotY: 0 },
-leftRock:  { x: -1, y: 0.95, z: 0.80, s: 1.05, rotY: 1.4 },
-rightRock: { x: 1.2,  y: 0.95, z: 0.80, s: 1.05, rotY: -1.4 },
+leftRock:  { x: -0.80, y: 0.95, z: 0.80, s: 1.05, rotY: 2 },
+rightRock: { x: 1.2,  y: 0.95, z: 0.80, s: 1.05, rotY: -2 },
 },
 7: {
 sofa:   { x: 0.35, y: 0,    z: 0,    s: 1,    rotY: 0 },
